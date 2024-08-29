88090f353f3b5a47140ca6bc638635f0f8578e91
Sami Adam <sami2010mohamed@gmail.com>
2024-08-29 20:37:02 +0300

    Enhancements

--- src/components/common/FormBaseLayout.jsx
+++ src/components/common/FormBaseLayout.jsx
@@ -1,10 +1,11 @@
-import { Box, Card } from "@mui/joy";
+import { Box, Card, useTheme } from "@mui/joy";
 import { FormBackButton } from "./Buttons";
 import LoadingPage from "./LaodingPage";
 
 
 
 export default function FormBaseLayout({ children, loading=false }) {
+    const theme = useTheme();
     return (
         <Box flex={1} sx={{ width: "100%"}}>
             {loading && <LoadingPage/> ||
@@ -20,6 +21,7 @@ export default function FormBaseLayout({ children, loading=false }) {
                 width: { xs: "90%", md: "70%" },
                 mt: { xs: 10, md: 4 },
                 boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
+                backgroundColor: theme.palette.mode == "light" ? "white" : "black",
                 
                 }}>
                 <FormBackButton/>
