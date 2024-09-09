77de450558bf1ee15621f4ea3b73cac7f78571f7
sadam <sami2010mohamed@gmail.com>
2024-09-09 16:07:12 +0300

    Error Boundary

--- src/components/common/ErrorBoundary.jsx
+++ src/components/common/ErrorBoundary.jsx
@@ -4,26 +4,23 @@ import React, { Component } from 'react';
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false, error: null };
+    this.state = { hasError: false };
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render shows the fallback UI.
-    return { hasError: true, error };
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
-    console.error("Uncaught error:", error, errorInfo);
+    console.log(error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      return (
-        <div style={{ display:"flex", justifyContent:"center", alignItems: "center"}}>
-          Something went wrong: {this.state.error.message}
-        </div>
-      )
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
     }
 
     return this.props.children; 
